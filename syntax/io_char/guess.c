2bff14fe75eb6a53bc4a95f2719c4a6cbf645faa
Coin <duanxian0403@163.com>
2016-02-14 10:32:42 +0800

    ~

---
+++ syntax/io_char/guess.c
@@ -0,0 +1,22 @@
+//
+// Created by coinxu on 12/12/15.
+//
+
+#include "guess.h"
+#include "stdio.h"
+
+int main(void) {
+    int guess = 1;
+    printf("Pick an integer from 1 to 100. I will try to guess ");
+    printf("it.\nRespond with a y if my guess is rigth and with");
+    printf("\nan n if it is wrong.\n");
+
+    printf("Uh...is your number %d?\n", guess);
+
+    while (getchar() != 'y'){
+        printf("Well, then. is it %d?\n", ++guess);
+    }
+
+    printf("I kew I could do it!\n");
+    return 0;
+}
